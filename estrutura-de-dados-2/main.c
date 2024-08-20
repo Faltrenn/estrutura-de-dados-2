9f358df690df5a08680f5df0651f50ea1f6b3958
Emanuel Alves <manelzaum@icloud.com>
2024-08-20 15:50:55 -0300

    Initial Commit

---
+++ estrutura-de-dados-2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  estrutura-de-dados-2
+//
+//  Created by Emanuel on 20/08/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
